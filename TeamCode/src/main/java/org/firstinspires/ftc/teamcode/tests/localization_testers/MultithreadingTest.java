ac9348510171174a76dd4a780e3534a46465198c
mura2432 <houcheng.jg@gmail.com>
2025-12-15 19:31:53 -0800

    MultithreadingTest created

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/localization_testers/MultithreadingTest.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode.tests.localization_testers;
+
+import static org.firstinspires.ftc.teamcode.tests.localization_testers.MultithreadingTest.opModeStartTime;
+
+import android.util.Log;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+class ControlHub implements Runnable {
+    DcMotorEx m;
+    boolean triggered = false;
+    public ControlHub(DcMotorEx m) { this.m = m; }
+
+    public void run() {
+        if (System.currentTimeMillis() - opModeStartTime > 5000 && !triggered) {
+            int pos = m.getCurrentPosition();
+            Log.i ("THREADING - Control", System.currentTimeMillis() - opModeStartTime + "");
+            triggered = true;
+        }
+    }
+}
+
+class ExpansionHub implements  Runnable {
+    DcMotorEx m;
+    boolean triggered = false;
+    public ExpansionHub(DcMotorEx m) { this.m = m; }
+
+    public void run() {
+        if (System.currentTimeMillis() - opModeStartTime > 5000 && !triggered) {
+            int pos = m.getCurrentPosition();
+            Log.i ("THREADING - Expansion", System.currentTimeMillis() - opModeStartTime + "");
+            triggered = true;
+        }
+    }
+}
+
+@TeleOp
+public class MultithreadingTest extends LinearOpMode {
+    public static double opModeStartTime = System.currentTimeMillis();
+
+    public void runOpMode(){
+        DcMotorEx m1 = hardwareMap.get(DcMotorEx.class, "motor1");
+        DcMotorEx m2 = hardwareMap.get(DcMotorEx.class, "motor2");
+
+        ControlHub chub = new ControlHub(m1);
+        ExpansionHub exhub = new ExpansionHub(m2);
+
+        while (opModeInInit()){ opModeStartTime = System.currentTimeMillis(); }
+
+        while (!isStopRequested()) {
+            chub.run();
+            exhub.run();
+        }
+    }
+}
